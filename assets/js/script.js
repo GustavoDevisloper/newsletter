42ec2e99a09ad99b287183f208d3a8754954dc5e
GustavoDevisloper <morenubrabo@gmail.com>
2023-10-04 21:01:39 -0300

    Backend #1

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+  // Adicione um manipulador de evento de clique ao botão
+  document.getElementById("button").addEventListener("click", function() {
+    // Simule o clique no botão de submit oculto
+    document.getElementById("meuInput").click();
+  });
